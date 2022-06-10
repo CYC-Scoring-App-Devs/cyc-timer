254c5ecc4dd3801bda52152d2b04654d7f843412
Adam Bowman <adamcbowman@gmail.com>
2022-06-10 15:45:42 -0300

    Update [id].js
    fix direct nav issue

--- pages/course-list/[id].js
+++ pages/course-list/[id].js
@@ -1,44 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const RaceID = () => {
-    const [courseDetails, setCourseDetails] = useState({});
-    const [loading, setLoading] = useState(true);
+  const [courseDetails, setCourseDetails] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    const router = useRouter()
-    const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
-    useEffect(() => {
-        axios.get("/api/courses/course-list").then((response) => {
-          //get details for id
-            const resArray = response.data.filter((course) => course.id === id)
-            setCourseDetails(resArray)
-            setLoading(false)
-        });
-      }, [id]);
+  useEffect(() => {
+    try {
+      axios.get("/api/courses/course-list").then((response) => {
+        if (id) {
+          const resArray = response.data.filter((course) => course.id === id);
+          setCourseDetails(resArray);
+          setLoading(false);
+          if (resArray.length === 0) {
+            setError(true);
+          }
+        }
+      });
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  }, [id]);
 
-    return (
-        <div className="">
-            {loading ? (
-                <div className="text-center p-12">
-                    <span>Loading...</span>
-                </div>
-            ) : (
-                <div className="text-center">
-                    <h1 className="pt-6 text-xl font-bold">Course: {courseDetails[0].name}</h1>
-                    <p className="p-2 text-xs">Length: ~{courseDetails[0].distance}NM</p>
-                    <ul className="list-disc p-3 border">
-                    {courseDetails[0].course.map((mark) => (
-                        <li key={mark} className="p-2">
-                            {mark}
-                        </li>
-                    ))}
-                    </ul>
-                </div>
-            )}
+  return (
+    <div className="">
+      {loading && (
+        <div className="text-center p-12">
+          <span>Loading...</span>
         </div>
-    )
-}
+      )}
 
-export default RaceID
+      {error && (
+        <div className="text-center p-12">
+          <span>Error...</span>
+        </div>
+      )}
+
+      {!loading && !error && (
+        <div className="text-center">
+          <h1 className="pt-6 text-xl font-bold">
+            Course: {courseDetails[0].name}
+          </h1>
+          <p className="p-2 text-xs">Length: ~{courseDetails[0].distance}NM</p>
+          <ul className="list-disc p-3 border">
+            {courseDetails[0].course.map((mark) => (
+              <li key={mark} className="p-2">
+                {mark}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RaceID;
