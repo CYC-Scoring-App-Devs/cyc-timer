66fa8bef0ea7cd9cdf3eb00cf5e7179ad1962e2a
Adam Bowman <adamcbowman@gmail.com>
2022-06-17 07:44:33 -0300

    coursedetails getStaticProps/paths

--- pages/course-list/[id].js
+++ pages/course-list/[id].js
@@ -1,80 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 import axios from "axios";
 
-
-const CourseId = () => {
-  const [courseDetails, setCourseDetails] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    try {
-      axios.get("/api/courses/course-list").then((response) => {
-        if (id) {
-          const resArray = response.data.filter((course) => course.id === id);
-          setCourseDetails(resArray);
-          setLoading(false);
-          if (resArray.length === 0) {
-            setError(true);
-          }
-        }
-      });
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-  }, [id]);
+const CourseId = ({ courseDetails }) => {
 
   return (
+    {courseDetails} && (
     <div className="min-h-screen">
-      {loading && (
-        <div className="text-center p-12">
-          <span>Loading...</span>
-        </div>
-      )}
 
-      {error && (
-        <div className="text-center p-12">
-          <span>Error...</span>
-        </div>
-      )}
-
-      {!loading && !error && (
         <div className="flex flex-col items-center">
-        <div className="text-center ">
-          <Image src="/logo.png" width={175} height={100} alt="cyc logo" className=""/>
-          <h1 className="pt-6 text-xl font-bold">
-            Course: {courseDetails[0].name}
-          </h1>
-          <p className="p-2 text-xs">Length: ~{courseDetails[0].distance}NM</p>
-          <ul className="list-disc p-3 border">
-            {courseDetails[0].course.map((mark, index) => (
-              <li key={index} className="p-2">
-                {mark}
-              </li>
-            ))}
-          </ul>
-          {courseDetails[0]?.map && (
-          <div className="border">
-          <img src={courseDetails[0].map} width={500} height={500} alt="course image" className=""/>
+          <div className="text-center ">
+            <Image
+              src="/logo.png"
+              width={175}
+              height={100}
+              alt="cyc logo"
+              className=""
+            />
+            <h1 className="pt-6 text-xl font-bold">
+              Course: {courseDetails[0].name}
+            </h1>
+            <p className="p-2 text-xs">
+              Length: ~{courseDetails[0].distance}NM
+            </p>
+            <ul className="list-disc p-3 border">
+              {courseDetails[0].course.map((mark, index) => (
+                <li key={index} className="p-2">
+                  {mark}
+                </li>
+              ))}
+            </ul>
+            {courseDetails[0]?.map && (
+              <div className="border">
+                <Image
+                  src={courseDetails[0].map}
+                  width={500}
+                  height={500}
+                  alt="course image"
+                  className=""
+                />
+              </div>
+            )}
           </div>
-          )}
         </div>
-        </div>
-      )}
-    <div className="text-center p-3">
+ 
+      <div className="text-center p-3">
         <button className="bg-transparent hover:bg-primary text-primary hover:text-white py-2 px-4 border border-primary hover:border-transparent">
-        <Link href="/course-list">Course List</Link>
+          <Link href="/course-list">Course List</Link>
         </button>
+      </div>
     </div>
-    </div>
+    )
   );
 };
+
+export async function getStaticPaths() {
+  const res = await axios.get("https://cyc-timer.herokuapp.com/api/courses/course-list");
+  const paths = res.data.map((course) => ({ params: { id: course.id } }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await axios.get(`https://cyc-timer.herokuapp.com/api/courses/course-list/`);
+  const courseDetails = res.data.filter((course) => course.id === params.id);
+  return { props: { courseDetails } };
+}
 
 export default CourseId;
